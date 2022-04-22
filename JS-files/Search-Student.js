3abd496702d1ab0dab663e2632a2025b277ef915
DiaaEssam <102432512+DiaaEssam@users.noreply.github.com>
2022-04-22 14:05:18 +0200

    Update Search-Student.js

--- JS-files/Search-Student.js
+++ JS-files/Search-Student.js
@@ -11,50 +11,46 @@ document.getElementById("row4").style.display = "none";
 
 function show() {
     var nameinput = document.querySelector("#name-entered").value;
-
-    if (nameinput == names[0]) {
-        document.getElementById("row1").style.display = "table-row";
-        document.getElementById("row2").style.display = "table-row";
-        document.getElementById("row3").style.display = "none";
-        document.getElementById("row4").style.display = "none";
-        document.getElementById("no-student").style.display = "none";
-
-    } else if (
-        nameinput == names[1]
-    ) {
-        document.getElementById("row1").style.display = "table-row";
-        document.getElementById("row3").style.display = "table-row";
-        document.getElementById("row2").style.display = "none";
-        document.getElementById("row4").style.display = "none";
-        document.getElementById("no-student").style.display = "none";
-
-    } else if (
-        nameinput == names[2]
-    ) {
-        document.getElementById("row1").style.display = "table-row";
-        document.getElementById("row4").style.display = "table-row";
-        document.getElementById("row2").style.display = "none";
-        document.getElementById("row3").style.display = "none";
-        document.getElementById("no-student").style.display = "none";
-
-    } else if (nameinput == "") {
-        document.getElementById("row1").style.display = "none";
-        document.getElementById("row2").style.display = "none";
-        document.getElementById("row3").style.display = "none";
-        document.getElementById("row4").style.display = "none";
-        document.getElementById("no-student").style.textAlign = "center";
-        document.getElementById("no-student").innerHTML =
-            "please enter a name";
-
-    }
-
-    else {
-        document.getElementById("row1").style.display = "none";
-        document.getElementById("row2").style.display = "none";
-        document.getElementById("row3").style.display = "none";
-        document.getElementById("row4").style.display = "none";
-        document.getElementById("no-student").style.textAlign = "center";
-        document.getElementById("no-student").innerHTML =
-            "the student you searched about does not exist";
+    switch (nameinput) {
+        case names[0]:
+            document.getElementById("row1").style.display = "table-row";
+            document.getElementById("row2").style.display = "table-row";
+            document.getElementById("row3").style.display = "none";
+            document.getElementById("row4").style.display = "none";
+            document.getElementById("no-student").style.display = "none";
+            break;
+        case names[1]:
+            document.getElementById("row1").style.display = "table-row";
+            document.getElementById("row3").style.display = "table-row";
+            document.getElementById("row2").style.display = "none";
+            document.getElementById("row4").style.display = "none";
+            document.getElementById("no-student").style.display = "none";
+            break;
+        case names[2]:
+            document.getElementById("row1").style.display = "table-row";
+            document.getElementById("row4").style.display = "table-row";
+            document.getElementById("row2").style.display = "none";
+            document.getElementById("row3").style.display = "none";
+            document.getElementById("no-student").style.display = "none";
+            break;
+        case "":
+            document.getElementById("row1").style.display = "none";
+            document.getElementById("row2").style.display = "none";
+            document.getElementById("row3").style.display = "none";
+            document.getElementById("row4").style.display = "none";
+            document.getElementById("no-student").style.display = "block";
+            document.getElementById("no-student").style.textAlign = "center";
+            document.getElementById("no-student").innerHTML =
+                "please enter a name";
+            break;
+        default:
+            document.getElementById("row1").style.display = "none";
+            document.getElementById("row2").style.display = "none";
+            document.getElementById("row3").style.display = "none";
+            document.getElementById("row4").style.display = "none";
+            document.getElementById("no-student").style.display = "block";
+            document.getElementById("no-student").style.textAlign = "center";
+            document.getElementById("no-student").innerHTML =
+                "the student you searched about does not exist";
     }
 }
