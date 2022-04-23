477be4a3a966d7571842756babbd7df75dc7da03
Crispy <b1063629@gmail.com>
2022-04-23 09:10:04 +0200

    Homepage/phone no./departement handeled
    @Le-Wolfie U should handle the departement field i types the code but in comments to ur code u should handle it with the appropriate CSS u have ,Also do the same in edit students (for the CSS)  .
    
    also added a hover over the student affairs homepage but still working on better images bec mesh ha5od el sewar de copy paste akid 7ata law shaklaha 7lw --sad--

--- java script/Search-Student.js
+++ java script/Search-Student.js
@@ -1,7 +1,6 @@
-
-const names = []
+const names = [];
 for (var i = 0; i < 9; i += 3) {
-    names.push(document.getElementsByTagName("td")[i].innerHTML)
+  names.push(document.getElementsByTagName("td")[i].innerHTML);
 }
 
 document.getElementById("row1").style.display = "none";
@@ -10,47 +9,46 @@ document.getElementById("row3").style.display = "none";
 document.getElementById("row4").style.display = "none";
 
 function show() {
-    var nameinput = document.querySelector("#name-entered").value;
-    switch (nameinput) {
-        case names[0]:
-            document.getElementById("row1").style.display = "table-row";
-            document.getElementById("row2").style.display = "table-row";
-            document.getElementById("row3").style.display = "none";
-            document.getElementById("row4").style.display = "none";
-            document.getElementById("no-student").style.display = "none";
-            break;
-        case names[1]:
-            document.getElementById("row1").style.display = "table-row";
-            document.getElementById("row3").style.display = "table-row";
-            document.getElementById("row2").style.display = "none";
-            document.getElementById("row4").style.display = "none";
-            document.getElementById("no-student").style.display = "none";
-            break;
-        case names[2]:
-            document.getElementById("row1").style.display = "table-row";
-            document.getElementById("row4").style.display = "table-row";
-            document.getElementById("row2").style.display = "none";
-            document.getElementById("row3").style.display = "none";
-            document.getElementById("no-student").style.display = "none";
-            break;
-        case "":
-            document.getElementById("row1").style.display = "none";
-            document.getElementById("row2").style.display = "none";
-            document.getElementById("row3").style.display = "none";
-            document.getElementById("row4").style.display = "none";
-            document.getElementById("no-student").style.display = "block";
-            document.getElementById("no-student").style.textAlign = "center";
-            document.getElementById("no-student").innerHTML =
-                "please enter a name";
-            break;
-        default:
-            document.getElementById("row1").style.display = "none";
-            document.getElementById("row2").style.display = "none";
-            document.getElementById("row3").style.display = "none";
-            document.getElementById("row4").style.display = "none";
-            document.getElementById("no-student").style.display = "block";
-            document.getElementById("no-student").style.textAlign = "center";
-            document.getElementById("no-student").innerHTML =
-                "the student you searched about does not exist";
-    }
+  var nameinput = document.querySelector("#name-entered").value;
+  switch (nameinput) {
+    case names[0]:
+      document.getElementById("row1").style.display = "table-row";
+      document.getElementById("row2").style.display = "table-row";
+      document.getElementById("row3").style.display = "none";
+      document.getElementById("row4").style.display = "none";
+      document.getElementById("no-student").style.display = "none";
+      break;
+    case names[1]:
+      document.getElementById("row1").style.display = "table-row";
+      document.getElementById("row3").style.display = "table-row";
+      document.getElementById("row2").style.display = "none";
+      document.getElementById("row4").style.display = "none";
+      document.getElementById("no-student").style.display = "none";
+      break;
+    case names[2]:
+      document.getElementById("row1").style.display = "table-row";
+      document.getElementById("row4").style.display = "table-row";
+      document.getElementById("row2").style.display = "none";
+      document.getElementById("row3").style.display = "none";
+      document.getElementById("no-student").style.display = "none";
+      break;
+    case "":
+      document.getElementById("row1").style.display = "none";
+      document.getElementById("row2").style.display = "none";
+      document.getElementById("row3").style.display = "none";
+      document.getElementById("row4").style.display = "none";
+      document.getElementById("no-student").style.display = "block";
+      document.getElementById("no-student").style.textAlign = "center";
+      document.getElementById("no-student").innerHTML = "please enter a name";
+      break;
+    default:
+      document.getElementById("row1").style.display = "none";
+      document.getElementById("row2").style.display = "none";
+      document.getElementById("row3").style.display = "none";
+      document.getElementById("row4").style.display = "none";
+      document.getElementById("no-student").style.display = "block";
+      document.getElementById("no-student").style.textAlign = "center";
+      document.getElementById("no-student").innerHTML =
+        "the student you searched about does not exist";
+  }
 }
