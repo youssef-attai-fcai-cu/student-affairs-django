27d743378dc344d2a3bb2e3d39a0bc736d977f8c
DiaaEssam <102432512+DiaaEssam@users.noreply.github.com>
2022-04-24 00:09:04 +0200

    fake database added

--- java script/database.js
+++ java script/database.js
@@ -1,8 +1,61 @@
+class student {
+    constructor(
+      ID,
+      name,
+      date,
+      gpa,
+      gender,
+      level,
+      departement,
+      Email,
+      mobile,
+      status
+    ) {
+      this.data = [
+        ID,
+        name,
+        date,
+        gpa,
+        gender,
+        level,
+        departement,
+        Email,
+        mobile,
+        status,
+      ];
+    }
+  }
+  var dummyData = [];
+  dummyData[0] = new student(20200, "ahmed mohamed", "10-5-2000", 4, "male", 1, "computer science", "ahmed@", "00000000", true);
+  dummyData[1] = new student(20200, "mohamed", "10-5-2000", 4, "male", 1, "computer science", "mohamed@", "00000000", true);
+  dummyData[2] = new student(20200, "sherif", "10-5-2000", 4, "male", 1, "computer science", "sherif@", "00000000", false);
+
+  /*----------------------------------------------------------------------------Search-Student--------------------------------------------------------------------------*/
 const names = [];
-for (var i = 0; i < 9; i += 3) {
-  names.push(document.getElementsByTagName("td")[i].innerHTML);
+const IDs = []
+const dates = []
+const rows = []
+for (var i = 0, j = i + 1; i < dummyData.length; i++, j++) {
+  names[i] = dummyData[i].data[1]
+  IDs[i] = dummyData[i].data[0]
+  dates[i] = dummyData[i].data[2]
+  var row = document.createElement("tr")
+  row.setAttribute("id", `row${j + 1}`)
+  rows[i] = row
 }
 
+for (var i = 0; i < dummyData.length; i++) {
+  var tabledata_n = document.createElement("td")
+  tabledata_n.innerHTML = names[i]
+  rows[i].appendChild(tabledata_n)
+  var tabledata_ID = document.createElement("td")
+  tabledata_ID.innerHTML = IDs[i]
+  rows[i].appendChild(tabledata_ID)
+  var tabledata_date = document.createElement("td")
+  tabledata_date.innerHTML = dates[i]
+  rows[i].appendChild(tabledata_date)
+  document.querySelector("#SS-tbody").appendChild(rows[i])
+}
 document.getElementById("row1").style.display = "none";
 document.getElementById("row2").style.display = "none";
 document.getElementById("row3").style.display = "none";
@@ -17,6 +70,8 @@ function alert_no_s()
 }
 
 function show() {
+
+
   var nameinput = document.querySelector("#name-entered").value;
   switch (nameinput.toLowerCase()) {
     case names[0].toLowerCase():
--- java script/view-students.js
+++ java script/view-students.js
@@ -4,37 +4,7 @@ checkList.getElementsByClassName("anchor")[0].onclick = function (evt) {
     checkList.classList.remove("visible");
   else checkList.classList.add("visible");
 };
-class student {
-  constructor(
-    ID,
-    name,
-    date,
-    gpa,
-    gender,
-    level,
-    departement,
-    Email,
-    mobile,
-    status
-  ) {
-    this.data = [
-      ID,
-      name,
-      date,
-      gpa,
-      gender,
-      level,
-      departement,
-      Email,
-      mobile,
-      status,
-    ];
-  }
-}
-var dummyData=[];
-dummyData[0]=new student(20200,"ahmed mohamed","10-5-2000",4,"male",1,"computer science","ahmed@","00000000",true);
-dummyData[1]=new student(20200,"mohamed","10-5-2000",4,"male",1,"computer science","mohamed@","00000000",true);
-dummyData[2]=new student(20200,"sherif","10-5-2000",4,"male",1,"computer science","sherif@","00000000",false);
+
 function displayStudent(student) {
   var row = document.createElement("tr");
   for (var i = 0; i < 10; i++) {
@@ -73,3 +43,9 @@ function showhide(obj, name) {
 for (var i = 0; i < 3; i++) {
   displayStudent(dummyData[i]);
 }
+
+
+
+
+
+
