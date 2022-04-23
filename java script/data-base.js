19d718ec4b66a50ba9269078f02d0c942b7d929f
Crispy <b1063629@gmail.com>
2022-04-23 19:47:03 +0200

    Added-database.js

---
+++ java script/data-base.js
@@ -0,0 +1,37 @@
+var dummyData = [];
+dummyData[0] = new student(
+  20200,
+  "ahmed mohamed",
+  "10-5-2000",
+  4,
+  "male",
+  1,
+  "computer science",
+  "ahmed@",
+  "00000000",
+  true
+);
+dummyData[1] = new student(
+  20200,
+  "mohamed",
+  "10-5-2000",
+  4,
+  "male",
+  1,
+  "computer science",
+  "mohamed@",
+  "00000000",
+  true
+);
+dummyData[2] = new student(
+  20200,
+  "sherif",
+  "10-5-2000",
+  4,
+  "male",
+  1,
+  "computer science",
+  "sherif@",
+  "00000000",
+  false
+);
