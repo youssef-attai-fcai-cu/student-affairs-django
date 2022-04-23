c9b6d381f67739ae71a905b4cf64b087fa301e7f
youssef-attai <youssefgalalnazem@gmail.com>
2022-04-23 19:59:31 +0200

    Removed  database

--- java script/view-students.js
+++ java script/view-students.js
@@ -31,10 +31,10 @@ class student {
     ];
   }
 }
-// var dummyData=[];
-// dummyData[0]=new student(20200,"ahmed mohamed","10-5-2000",4,"male",1,"computer science","ahmed@","00000000",true);
-// dummyData[1]=new student(20200,"mohamed","10-5-2000",4,"male",1,"computer science","mohamed@","00000000",true);
-// dummyData[2]=new student(20200,"sherif","10-5-2000",4,"male",1,"computer science","sherif@","00000000",false);
+var dummyData=[];
+dummyData[0]=new student(20200,"ahmed mohamed","10-5-2000",4,"male",1,"computer science","ahmed@","00000000",true);
+dummyData[1]=new student(20200,"mohamed","10-5-2000",4,"male",1,"computer science","mohamed@","00000000",true);
+dummyData[2]=new student(20200,"sherif","10-5-2000",4,"male",1,"computer science","sherif@","00000000",false);
 function displayStudent(student) {
   var row = document.createElement("tr");
   for (var i = 0; i < 10; i++) {
