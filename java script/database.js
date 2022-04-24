496ed13ac9ba94f69bc3c1ccba7636241caa0934
DiaaEssam <102432512+DiaaEssam@users.noreply.github.com>
2022-04-24 19:52:44 +0200

    some comments removed

--- java script/database.js
+++ java script/database.js
@@ -30,6 +30,8 @@ dummyData[0] = new student(20200, "ahmed mohamed", "10-5-2000", 4, "male", 1, "c
 dummyData[1] = new student(20200, "mohamed", "10-5-2000", 4, "male", 1, "computer science", "mohamed@", "00000000", true);
 dummyData[2] = new student(20200, "sherif", "10-5-2000", 4, "male", 1, "computer science", "sherif@", "00000000", false);
 
+
+
 /*----------------------------------------------------------------------------Search-Student--------------------------------------------------------------------------*/
 const names = [];
 const IDs = []
