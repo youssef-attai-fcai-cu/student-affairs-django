d1b524cb46a1966a6258f9fb3cbf39521dd712f6
DiaaEssam <102432512+DiaaEssam@users.noreply.github.com>
2022-04-24 14:09:17 +0200

    enhanced code for database

--- java script/database.js
+++ java script/database.js
@@ -1,5 +1,17 @@
 class student {
-    constructor(
+  constructor(
+    ID,
+    name,
+    date,
+    gpa,
+    gender,
+    level,
+    departement,
+    Email,
+    mobile,
+    status
+  ) {
+    this.data = [
       ID,
       name,
       date,
@@ -9,32 +21,22 @@ class student {
       departement,
       Email,
       mobile,
-      status
-    ) {
-      this.data = [
-        ID,
-        name,
-        date,
-        gpa,
-        gender,
-        level,
-        departement,
-        Email,
-        mobile,
-        status,
-      ];
-    }
+      status,
+    ];
   }
-  var dummyData = [];
-  dummyData[0] = new student(20200, "ahmed mohamed", "10-5-2000", 4, "male", 1, "computer science", "ahmed@", "00000000", true);
-  dummyData[1] = new student(20200, "mohamed", "10-5-2000", 4, "male", 1, "computer science", "mohamed@", "00000000", true);
-  dummyData[2] = new student(20200, "sherif", "10-5-2000", 4, "male", 1, "computer science", "sherif@", "00000000", false);
+}
+var dummyData = [];
+dummyData[0] = new student(20200, "ahmed mohamed", "10-5-2000", 4, "male", 1, "computer science", "ahmed@", "00000000", true);
+dummyData[1] = new student(20200, "mohamed", "10-5-2000", 4, "male", 1, "computer science", "mohamed@", "00000000", true);
+dummyData[2] = new student(20200, "sherif", "10-5-2000", 4, "male", 1, "computer science", "sherif@", "00000000", false);
 
-  /*----------------------------------------------------------------------------Search-Student--------------------------------------------------------------------------*/
+/*----------------------------------------------------------------------------Search-Student--------------------------------------------------------------------------*/
 const names = [];
 const IDs = []
 const dates = []
 const rows = []
+
+
 for (var i = 0, j = i + 1; i < dummyData.length; i++, j++) {
   names[i] = dummyData[i].data[1]
   IDs[i] = dummyData[i].data[0]
@@ -56,54 +58,43 @@ for (var i = 0; i < dummyData.length; i++) {
   rows[i].appendChild(tabledata_date)
   document.querySelector("#SS-tbody").appendChild(rows[i])
 }
-document.getElementById("row1").style.display = "none";
-document.getElementById("row2").style.display = "none";
-document.getElementById("row3").style.display = "none";
-document.getElementById("row4").style.display = "none";
-function alert_empty()
-{
+
+
+for (var i = 0; i < dummyData.length + 1; i++) {
+  document.getElementById(`row${i + 1}`).style.display = "none";
+}
+
+
+function alert_empty() {
   alert("Please enter a name")
 }
-function alert_no_s()
-{
+function alert_no_s() {
   alert("the student you searched about does not exist")
 }
 
 function show() {
 
+  for (var i = 0; i < dummyData.length + 1; i++) {
+    document.getElementById(`row${i + 1}`).style.display = "none";
+  }
 
   var nameinput = document.querySelector("#name-entered").value;
-  switch (nameinput.toLowerCase()) {
-    case names[0].toLowerCase():
-      document.getElementById("row1").style.display = "table-row";
-      document.getElementById("row2").style.display = "table-row";
-      document.getElementById("row3").style.display = "none";
-      document.getElementById("row4").style.display = "none";
-      break;
-    case names[1].toLowerCase():
-      document.getElementById("row1").style.display = "table-row";
-      document.getElementById("row3").style.display = "table-row";
-      document.getElementById("row2").style.display = "none";
-      document.getElementById("row4").style.display = "none";
-      break;
-    case names[2].toLowerCase():
-      document.getElementById("row1").style.display = "table-row";
-      document.getElementById("row4").style.display = "table-row";
-      document.getElementById("row2").style.display = "none";
-      document.getElementById("row3").style.display = "none";
-      break;
-    case "":
-      document.getElementById("row1").style.display = "none";
-      document.getElementById("row2").style.display = "none";
-      document.getElementById("row3").style.display = "none";
-      document.getElementById("row4").style.display = "none";
-      alert_empty()
-      break;
-    default:
-      document.getElementById("row1").style.display = "none";
-      document.getElementById("row2").style.display = "none";
-      document.getElementById("row3").style.display = "none";
-      document.getElementById("row4").style.display = "none";
-      alert_no_s()
+  var found = true
+
+  if (nameinput == "") {
+    found = false
+    alert_empty()
+  }
+  else {
+    for (var i = 0, j = 2; i < names.length; i++, j++) {
+      if (nameinput.toString().toLowerCase() == names[i].toString().toLowerCase()) {
+        found = false
+        document.getElementById("row1").style.display = "table-row";
+        document.getElementById(`row${j}`).style.display = "table-row";
+      }
+    }
+  }
+  if (found) {
+    alert_no_s()
   }
 }
