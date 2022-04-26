3d12ff8e764d327461d6005d39675491d7b94e91
GotchaPR0 <karimw11@yahoo.com>
2022-04-26 17:00:22 +0200

    Assign

--- java script/Assign-Department.js
+++ java script/Assign-Department.js
@@ -32,32 +32,98 @@ function entered_data(){
     confirm("Are you sure you want to assign this department?");
 }
 
-function assignFunction()
+/*function assignFunction()
 {
     let nameField = document.querySelector(`#student-name`).value;
 
     let idField = document.querySelector(`#student-id`).value;
 
-    //let falsefield = document.getElementsByClassName("selected").value;
+    let falsefield = document.getElementsByClassName("selected").value;
 
-    //let radioFields = document.querySelectorAll(`.radio`).value;
+    let radioFields = document.querySelector(".radio").value;
+
+    var deptData = [];
+    deptData[0] = document.querySelector("#CS").value;
+    deptData[1] = document.querySelector("#AI").value;
+    deptData[2] = document.querySelector("#IT").value;
+    deptData[3] = document.querySelector("#IS").value;
+    deptData[4] = document.querySelector("#DS").value;
+
+    let department
+
+    for(var i = 0 ; i < deptData.length ; i++)
+    {
+        if(deptData[i] != "")
+        {
+            department = deptData[i];
+        }
+    }
 
     if (nameField.length == 0 || idField.length == 0)
     {
+        isEmpty();    
+    }
+    else if (radioFields != "Computer Science" && radioFields != "Artificial Intelligence" && radioFields != "Information Technology" && radioFields != "Information Systems" && radioFields != "Decision Support")
+    {
         isEmpty();
-        return false;
-        /*if(falsefield == radioFields)
-        {
-            isEmpty();
-            return false;
-        }
-        else {
+    }
+    else{
         entered_data();
-        return true; }*/
+        return true; 
+    }
+
+    if(radioFields == "Computer Science")
+    {
+        isEmpty();
     }
     else 
     {
         entered_data();
         return true; 
     }
+}*/
+assignBtn = document.getElementById("optionsForm");
+
+assignBtn.addEventListener("submit", function(e){
+    e.preventDefault();
+})
+
+document.getElementById('assign-btn').onclick = function() {
+    var radios = document.getElementsByName('dept_choice');
+
+    let nameField = document.querySelector(`#student-name`).value;
+
+    let idField = document.querySelector(`#student-id`).value;
+    
+    if (nameField.length == 0 && idField.length == 0)
+    {
+        isEmpty();    
+    }
+    else
+    {
+        for (var radio of radios)
+        {
+            if (nameField.length != 0 && idField.length != 0) {
+                console.log("Outer IF loop")
+                if(radio.checked){
+                    console.log("Inner IF loop")
+                    alert(radio.value);
+                    confirm("Are you sure you want to assign this department?");
+                    return;
+                }
+            }
+            /*else if(nameField.length != 0 && idField.length != 0 && radio.unchecked){
+                isEmpty();
+            }*/
+        }
+    }
+        /*for (var radio of radios)
+        {
+            if (nameField.length != 0 && idField.length != 0 && radio.checked) {
+                alert("Department selected: ", radio.value);
+                confirm("Are you sure you want to assign this department?");
+                break;
+            }
+        }*/
 }
+
