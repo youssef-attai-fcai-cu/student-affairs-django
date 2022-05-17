92d036f3a71770fb717054bae7e7e53b3c86e995
Amr <lewolfje@gmail.com>
2022-05-17 14:36:23 +0200

    students can be added to the database, kill me

--- studentaffairs/sap/models.py
+++ studentaffairs/sap/models.py
@@ -1,18 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+
 class informations(models.Model):
-    #if bad thing happend change data from the migrations
-    IDS=models.IntegerField(blank=True,null=True,default=None)
-    name=models.CharField(max_length=500,default='')
-    date=models.IntegerField(blank=True,null=True,default=None)
-    gpa=models.FloatField(blank=True,null=True,default=None)
-    GENDER_MALE = 0
-    GENDER_FEMALE = 1
-    GENDER_CHOICES = [(GENDER_MALE, 'Male'), (GENDER_FEMALE, 'Female')]
-    gender = models.IntegerField(choices=GENDER_CHOICES)
-    level=models.IntegerField(blank=True,null=True,default=None)
-    departement=models.CharField(max_length=500,default='')
-    Email=models.CharField(max_length=500,default='')
-    mobile=models.CharField(max_length=500,default='')
-    status=models.CharField(max_length=500,default='')
+    # if bad thing happend change data from the migrations
+    name = models.CharField(max_length=500, default='')
+    date = models.CharField(max_length=500, default='')
+    gpa = models.FloatField(blank=True, null=True, default=None)
+    gender = models.CharField(max_length=500, default='')
+    level = models.CharField(max_length=500, default='')
+    department = models.CharField(max_length=500, default='')
+    email = models.CharField(max_length=500, default='')
+    mobile = models.CharField(max_length=500, default='')
+    status = models.CharField(max_length=500, default='')
