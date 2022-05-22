d97c74adf9a3da11691266e2c27694057f305f46
Amr <lewolfje@gmail.com>
2022-05-22 20:13:29 +0200

    added validation to search, disabled paste in search and student department

--- studentaffairs/static/assets/java script/Search-Student.js
+++ studentaffairs/static/assets/java script/Search-Student.js
@@ -1,22 +1,32 @@
-
+let validKeyCodes = [8, 9, 37, 38, 39, 40, 46];
 function alert_empty() {
-  alert("Please enter a name")
+  alert("Please enter a name");
 }
 function validation() {
   var nameinput = document.querySelector("#name-entered").value;
   if (nameinput == "") {
-    alert_empty()
+    alert_empty();
   }
 }
+function onlyLetterKey(evt) {
+  let ASCIICode = evt.which;
+  for (let i = 0; i < validKeyCodes.length; i++) {
+    if (ASCIICode == validKeyCodes[i]) {
+      return;
+    }
+  }
+  if ((ASCIICode >= 33 && ASCIICode <= 57) || ASCIICode == 192) {
+    evt.preventDefault();
+  } else if (ASCIICode >= 96 && ASCIICode <= 105) {
+    evt.preventDefault();
+  }
+}
+let searchInput = document.getElementById("name-entered");
+searchInput.addEventListener("keydown", onlyLetterKey);
 
 var el = document.getElementById("name-entered");
-    el.addEventListener("keypress", function(event) {
-      if (event.key === "Enter") {
-        event.preventDefault();
-      }
-    });
-
-
-
-
-
+el.addEventListener("keypress", function (event) {
+  if (event.key === "Enter") {
+    event.preventDefault();
+  }
+});
